cc290a462fee0c4d25509dd67f8c31359c72304d
jkarlsss <kolley320@gmail.com>
2026-06-02 20:58:58 +0800

    reset

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,8 +3,8 @@ import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 
 import * as Sentry from "@sentry/hono/bun";
-import chat from "./routes/chat.js";
-import sessions from "./routes/sessions.js";
+import chat from "./routes/chat";
+import sessions from "./routes/sessions";
 
 const app = new Hono();
 
