16770f63c14f1d00242a48d85181fc21f6e0366d
jkarlsss <kolley320@gmail.com>
2026-06-02 20:14:40 +0800

    origin main

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,8 +3,8 @@ import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 
 import * as Sentry from "@sentry/hono/bun";
-import chat from "./routes/chat";
-import sessions from "./routes/sessions";
+import chat from "./routes/chat.js";      // note .js
+import sessions from "./routes/sessions.js";
 
 const app = new Hono();
 
